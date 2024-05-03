24daba2b5dd4333eb7096e919ed556f2f0c96cd7
DESKTOP-L97VA5M\oshiz <oshizawadai.1192@gmail.com>
2024-05-03 09:41:50 +0900

    マテリアルを共有していても個別に設定できるようにした

--- UIGradation.cs
+++ UIGradation.cs
@@ -1,30 +1,100 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Image))]
-public class UIGradation : MonoBehaviour
+[RequireComponent(typeof(Graphic))]
+public class UIGradation : UIBehaviour, IMaterialModifier
 {
-    [SerializeField]
-    private Shader gradationShader;
-
     [SerializeField]
     private Vector2 leftPos = Vector2.left;
 
     [SerializeField]
     private Vector2 rightPos = Vector2.right;
 
-#if UNITY_EDITOR
-    private void OnValidate()
+    [NonSerialized]
+    private Graphic graphic;
+    public Graphic graphicProp => graphic ? graphic : graphic = GetComponent<Graphic>();
+
+    [NonSerialized]
+    private Material gradationMaterial;
+
+    private readonly int rotPropertyId = Shader.PropertyToID("_Rot");
+
+    private float rad;
+
+    protected override void OnEnable()
     {
-        Material mat = GetComponent<Image>().material;
-        mat.shader = gradationShader;
+        base.OnEnable();
+        if (graphicProp == null) return;
+        graphicProp.SetMaterialDirty();
+    }
 
-        Vector2 dt = rightPos - leftPos;
-        float rad = Mathf.Atan2(dt.y, dt.x);
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        if(gradationMaterial != null)
+        {
+            DestroyImmediate(gradationMaterial);
+        }
+        gradationMaterial = null;
 
-        mat.SetFloat("_Rot", rad);
+        if(graphicProp != null)
+        {
+            graphicProp.SetMaterialDirty();
+        }
+    }
+
+#if UNITY_EDITOR
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+
+        if (!IsActive() || graphicProp == null)
+        {
+            return;
+        }
+
+        graphicProp.SetMaterialDirty();
     }
 #endif
+
+    protected override void OnDidApplyAnimationProperties()
+    {
+        base.OnDidApplyAnimationProperties();
+        if (!IsActive() || graphicProp == null)
+        {
+            return;
+        }
+
+        graphicProp.SetMaterialDirty();
+    }
+
+    Material IMaterialModifier.GetModifiedMaterial(Material baseMaterial)
+    {
+        var isSameShader = baseMaterial.shader.name == graphicProp.material.shader.name;
+        if (IsActive() == false || graphic == null || !isSameShader)
+        {
+            return baseMaterial;
+        }
+
+        if(gradationMaterial == null)
+        {
+            gradationMaterial = new Material(baseMaterial);
+            gradationMaterial.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        gradationMaterial.CopyPropertiesFromMaterial(baseMaterial);
+
+        CalcRotation();
+
+        gradationMaterial.SetFloat(rotPropertyId, rad);
+        return gradationMaterial;
+    }
+
+    private void CalcRotation()
+    {
+        Vector2 dt = rightPos - leftPos;
+        rad = Mathf.Atan2(dt.y, dt.x);
+    }
 }
